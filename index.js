a0a6d0277236108df790fb9bc60d37aa6b085197
osama-abdullah <osama2kabdullah@gmail.com>
2022-09-26 14:21:02 +0600

    all voleenter topic api created

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.blfheza.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try {
+        client.connect();
+        const topicCollection = client.db("voleenter").collection("volenteer-activities");
+        
+        app.get('/volenteerTopics', async (req, res)=>{
+            const result = await topicCollection.find({});
+            const topics = await result.toArray();
+            res.send(topics);
+        })
+        
+    }
+    finally {
+        // client.close();
+    }
+}
+run().catch(console.dir)
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
 
 app.get('/', (req, res)=> {
     res.send('hello world')
